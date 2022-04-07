50f33b7580e1207d3ce2a7490d5a736c484fcc93
SweetsMarie <87447081+SweetsMarie@users.noreply.github.com>
2022-04-07 00:09:06 -0600

    Update server.lua
    Modified for QBCore

--- server.lua
+++ server.lua
@@ -1,41 +1,32 @@
 --MADE BY DareToDoyle#3835--
+--Modified by SweetsMarie--
 
-ESX                = nil
+QBCore = exports['qb-core']:GetCoreObject()
 
-TriggerEvent('esx:getSharedObject', function(obj) ESX = obj end)
-
-ESX.RegisterUsableItem('adrenaline', function(source)
-
-	local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    xPlayer.removeInventoryItem('adrenaline', 1)
+ QBCore.Functions.CreateUseableItem('adrenaline', function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    Player.Functions.RemoveItem('adrenaline', 1, itemslot)
 	TriggerClientEvent('D2D-Stims:adrenaline', source)
 	
 end)
 
-ESX.RegisterUsableItem('meldonin', function(source)
-
-	local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    xPlayer.removeInventoryItem('meldonin', 1)
+ QBCore.Functions.CreateUseableItem('meldonin', function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    Player.Functions.RemoveItem('meldonin', 1, itemslot)
 	TriggerClientEvent('D2D-Stims:meldonin', source)
 	
 end)
 
-ESX.RegisterUsableItem('anesthetic', function(source)
-
-	local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    xPlayer.removeInventoryItem('anesthetic', 1)
+ QBCore.Functions.CreateUseableItem('anesthetic', function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    Player.Functions.RemoveItem('anesthetic', 1, itemslot)
 	TriggerClientEvent('D2D-Stims:anesthetic', source)
 	
 end)
 
-ESX.RegisterUsableItem('ketamine', function(source)
-
-	local _source = source
-    local xPlayer = ESX.GetPlayerFromId(_source)
-    xPlayer.removeInventoryItem('ketamine', 1)
+ QBCore.Functions.CreateUseableItem('ketamine', function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    Player.Functions.RemoveItem('ketamine', 1, itemslot)
 	TriggerClientEvent('D2D-Stims:ketamine', source)
 	
 end)
